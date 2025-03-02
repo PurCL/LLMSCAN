90e08521644209fc47e9b2d8a8c24dcb5780060a
chengpeng-wang <15705513022@163.com>
2025-03-01 23:22:38 -0500

    update

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-source_bashrc() {
-  if [ -f "$HOME/.bashrc" ]; then
-    . "$HOME/.bashrc"
-  fi
-}
+# #!/bin/bash
+# source_bashrc() {
+#   if [ -f "$HOME/.bashrc" ]; then
+#     . "$HOME/.bashrc"
+#   fi
+# }
 
-source_bashrc
+# source_bashrc
 
 python3 scan.py \
   --language=C \
@@ -20,18 +20,3 @@ python3 scan.py \
   --project-path ../benchmark/C++ \
   --global-temperature 0.0 \
   --scanners metascan
-
-python3 scan.py \
-  --inference-model gpt-3.5-turbo-0125 \
-  --language Java \
-  --project-path ../benchmark/Java \
-  --global-temperature 0.0 \
-  --scanners metascan
-
-python3 scan.py \
-  --project-path ../benchmark/Python \
-  --language Python \
-  --inference-model gpt-3.5-turbo-0125 \
-  --global-temperature 0.0 \
-  --scanners metascan
-  
